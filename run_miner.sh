8edbf38ecc49780aad8b1ca4bd51fb6f1920893a
Maditwid <maditwid@gmail.com>
2024-10-19 11:59:07 +0700

    Add files via upload

---
+++ run_miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer.exe -c stratum+tcp://sg.vipor   .net:5040 -u RXnctRcqXtuntqQtARKe1q132v3ZskYcr9.HMinner -p x --cpu 8
+
+./hellminer.exe -c stratum+tcp://sg.vipor   .net:5040 -u RXnctRcqXtuntqQtARKe1q132v3ZskYcr9.HMinner -p x --cpu 8
